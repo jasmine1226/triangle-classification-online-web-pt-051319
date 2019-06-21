6d65a3cd14be73b5eb704f3e63b78184a3f3255e
Jasmine Chen <jasmine1226@gmail.com>
2019-06-21 07:00:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
+  attr_accessor :side_1, :side_2, :side_3
   # write code here
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    
+    return type
+  end
 end
