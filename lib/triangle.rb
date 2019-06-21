1304b24175a886daa01c916632f7bd3d5b450892
Jasmine Chen <jasmine1226@gmail.com>
2019-06-21 07:06:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,20 @@ class Triangle
   end
 
   def kind
-
+    if 
+      raise TriangleError
+    end
+    if @side_1 == @side_2 && @side_1 == @side_3
+      type = :equilateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      type = :isosceles
+    elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3
+      type = :scalene
+    end
     return type
+  end
+
+  class TriangleError < StandardError
+    
   end
 end
